71870499f02d918a79e6e68b47deae0339bc5a9e
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-19 21:17:03 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,19 +9,32 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
-
   post '/login' do
 
+   @user = User.find_by(username:params[:username])
+    if @user
+     if params[:password] == @user.password
+       session[:user_id] = @user.id
+       redirect to '/account'
+     else
+       redirect to '/'
+     end
+    else
+       redirect to '/error'
+    end
   end
 
   get '/account' do
-
+     erb :account
   end
 
   get '/logout' do
 
   end
 
+  get '/error' do
+    erb :error
+  end
+
 
 end
-
