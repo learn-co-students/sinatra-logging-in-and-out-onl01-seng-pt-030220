bfcc7fc25ad94c676320fb7883ce505f88fc080c
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-06-04 22:33:04 -0700

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -5,12 +5,13 @@ class Helpers
   end
 
   def self.is_logged_in?(session)
-    @logged_in = User.find(session[:user_id])
-    if !@logged_in
-      false
-    else
-      true
-    end
+    !!session[:user_id]
+    # @logged_in = User.find(session[:user_id])
+    # if !@logged_in
+    #   false
+    # else
+    #   true
+    # end
   end
 
 end
