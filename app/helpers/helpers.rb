1a9f39cf8d1dd5155332db9086d437e88e7ba008
VQuine <01010110.v@gmail.com>
2020-07-08 21:51:29 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
 class Helpers
+
+    def self.current_user(session) #parameter name  mattered...but why?
+        @user = User.find(session[:user_id])
+    end
+
+    def self.is_logged_in?(session)
+        !!session[:user_id]
+        #if the USER_ID inside the SESSION hash exists - return TRUE or FALSE
+        #!I dont understand how this will automatically compare itself to @user.id
+        #!REVIEW ----
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
-
-
+#* HANDLE RUBY LOGIC
+class User < ActiveRecord::Base
+    
+end
